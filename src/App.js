c2f3d5911c2ee6f0a99f205fdd59f9ff019d0072
nurbolatk <nurbolattk@gmail.com>
2021-03-02 22:37:13 +0600

    Setup project and send notifications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+function checkNotificationPromise() {
+  try {
+    Notification.requestPermission().then();
+  } catch (e) {
+    console.log("No promise");
+    return false;
+  }
+  console.log("Promise");
+
+  return true;
+}
+
+function send() {
+  new Notification("PIDR");
+}
 
 function App() {
+  const [permission, setPermission] = useState("default");
+  console.log("Notification" in window);
+
+  useEffect(askNotificationPermission, []);
+
+  function askNotificationPermission() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      console.log("This browser does not support notifications.");
+    } else {
+      if (checkNotificationPromise()) {
+        Notification.requestPermission().then((permission) => {
+          handlePermission(permission);
+        });
+      } else {
+        Notification.requestPermission(function (permission) {
+          handlePermission(permission);
+        });
+      }
+    }
+  }
+
+  function handlePermission(perm) {
+    setPermission(perm);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        disabled={permission === "granted"}
+        onClick={askNotificationPermission}
+      >
+        Request permisson
+      </button>
+      <button onClick={send}>Send notification</button>
     </div>
   );
 }
